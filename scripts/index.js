323cf63c5eb008b5ec3d55795912be4d4fa19988
zWing <371657110@qq.com>
2019-08-02 22:29:14 +0800

    feat: 🎸 update console.log

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,9 @@
 module.exports = (acyort) => {
+  console.log('starting...')
   acyort.util.outputHTML({
     template: 'index',
     path: 'index.html'
   })
-  console.log(acyort.config.get())
+  console.log('done')
   acyort.util.copySource()
 }
